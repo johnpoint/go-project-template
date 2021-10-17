69be977a904a9636aabec79be7e2118f861fb6ee
johnpoint <me@lvcshu.com>
2021-10-18 00:25:24 +0800

    feat: 输出依赖加载耗时

--- initHelper/init.go
+++ initHelper/init.go
@@ -16,21 +16,22 @@ type Helper struct {
 }
 
 func (i *Helper) Init(ctx context.Context) error {
+	startTime := time.Now()
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println("[init] Start")
 	if err := config.Config.ReadConfig(); err != nil {
 		panic(err)
 	}
 	for j := range i.Depends {
+		startSubTime := time.Now()
 		if i.Depends[j].GetEnable() {
-			fmt.Printf("[init] \u001B[1;32;40m%s\u001B[0m\n", i.Depends[j].GetName())
 			err := i.Depends[j].Init(ctx)
 			if err != nil {
 				return err
 			}
+			fmt.Printf("[init] \u001B[1;32;40m%s\u001B[0m at %d ms\n", i.Depends[j].GetName(), time.Now().UnixMilli()-startSubTime.UnixMilli())
 		}
 	}
-	fmt.Println("[init] Finish")
+	fmt.Println(fmt.Sprintf("[init] Finish at %d ms", time.Now().UnixMilli()-startTime.UnixMilli()))
 	return nil
 }
 
