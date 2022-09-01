f7ff4cb4e9b97e21a4a18c2a522f86376a67036a
johnpoint <me@lvcshu.com>
2022-09-01 10:03:00 +0800

    fix: init.sh

--- init.sh
+++ init.sh
@@ -7,13 +7,11 @@ fi
 if [ $# == 1 ]; then
   echo "[init] clean go mod"
   rm go.mod go.sum
-  echo "[init] create folder"
-  mkdir app app/controller cmd config
   echo "[init] create main file"
   sed -i 's/PROJECT_NAME/'$1'/g' app/controller/*
   sed -i 's/PROJECT_NAME/'$1'/g' cmd/*
   sed -i 's/PROJECT_NAME/'$1'/g' config/*
-  sed -i 's/PROJECT_NAME/'$1'/g' depend/*
+  sed -i 's/PROJECT_NAME/'$1'/g' component/*
   sed -i 's/PROJECT_NAME/'$1'/g' .gitignore
   sed -i 's/PROJECT_NAME/'$1'/g' PROJECT_NAME.go
   sed -i 's/PROJECT_NAME/'$1'/g' infra/error.go
